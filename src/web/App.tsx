d184f4bde9aa16192bc7f38cfbf446d771146d63
nakamaay <pcyama7ka@gmail.com>
2023-01-22 04:59:17 +0900

    first commit

--- src/web/App.tsx
+++ src/web/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react';
 import './App.css';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="container">
-      <h1>{count}</h1>
-      <button onClick={() => setCount((count) => count + 1)}>Count</button>
+      <h1>Hello.</h1>
     </div>
   );
 };
